d62acec8f2267321d14ea0b5930056cda9d61430
adrianwit <adrianwit@gmail.com>
2018-07-19 15:11:25 -0700

    updated test

--- unordered_collection_use_case/registry_test.go
+++ unordered_collection_use_case/registry_test.go
@@ -14,16 +14,32 @@ func TestRegistry_AsSlice(t *testing.T) {
 	}
 	var useCases = []*usecase(nil)
 
-	useCases = append(useCases , &usecase{
-		description:"overriding message",
-		messages:[]*Message{
+
+	useCases = append(useCases, &usecase{
+		description: "unordered message",
+		messages: []*Message{
+			NewMessage(20, "name 20"),
+			NewMessage(10, "name 10"),
+		},
+
+		expected: []interface{}{
+			assertly.IndexBy("ID"),
+			NewMessage(10, "name 10"),
+			NewMessage(20, "name 20"),
+		},
+	})
+
+
+	useCases = append(useCases, &usecase{
+		description: "overriding message",
+		messages: []*Message{
 			NewMessage(1, "dummy 1"),
 			NewMessage(2, "name 2"),
 			NewMessage(1, "name 1"),
 		},
-		expected:`[
+		expected: `[
 	  {
-		"@indexBy@":"id"
+		"@indexBy@":"ID"
 	  },
 	  {
 		"ID":1,
@@ -33,8 +49,7 @@ func TestRegistry_AsSlice(t *testing.T) {
 		"ID":2,
 		"Name":"name 2"
 	  }
-	]
-`,
+	]`,
 	})
 
 	for _, useCase := range useCases {
